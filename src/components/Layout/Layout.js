ace4bac83e12778f5656fbd4489cc04797988f0c
Rishabh Bajaj <bajajris@sheridancollege.ca>
2020-05-04 18:12:26 -0400

    added menu button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,20 +5,27 @@ import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false })
   }
-
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    });
+  }
   render() {
     return (
       <Aux>
-        <Toolbar />
-        <SideDrawer 
-        open={this.state.showSideDrawer} 
-        closed={this.sideDrawerClosedHandler} />
+        <Toolbar
+          DrawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
         <main className={classes.Content}>{this.props.children}</main>
       </Aux >
     );
