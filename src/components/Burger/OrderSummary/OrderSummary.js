ded740ddfeb74a09be318f7c1a009357e6e88679
Rishabh Bajaj <bajajris@sheridancollege.ca>
2020-05-07 14:46:11 -0400

    changed folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>
-                    {igKey}:
+class OrderSummary extends Component {
+    //could be a function component does not have to be a class component
+    componentDidUpdate() {
+        console.log("Order Summary")
+    }
+
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>
+                        {igKey}:
                 </span>
-                {props.ingredients[igKey]}
-            </li>
-        );
-    });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout!!</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>
-                CHECKOUT
+                    {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout!!</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>
+                    CHECKOUT
             </Button>
-        </Aux>
-    );
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import classes from "./app.module.css";
 import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
-import Layout from "../components/Layout/Layout";
+import Layout from "../hoc/Layout/Layout";
 class App extends Component {
   render() {
     return (
